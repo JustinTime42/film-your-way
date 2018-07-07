20c855dd20dbb24f0233df4698b0554e4e98d4ea
Justin Schneider <justincase18@msn.com>
2018-07-06 16:17:25 -0800

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = ({ logo, tagline }) => {
         <div>   
            <h2>{tagline}</h2>
             <div>
-                <iframe src={"//eepurl.com/dAlvpP"} />
+                <iframe src={"https://eepurl.com/dAlvpP"} />
             </div>
             
 
